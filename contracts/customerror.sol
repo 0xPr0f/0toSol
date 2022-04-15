62978cef19a9d6024c142f00ab0e7b307d2171ed
0xPr0f <0xprofoz@gmail.com>
2022-04-15 17:44:01 +0100

    first commit

---
+++ contracts/customerror.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8;
+// custom error
+
+contract VendingMachine {
+    address payable owner = payable(msg.sender);
+    /// @notice custom error e.g error Unauthorized(); must not be decleared in the contract
+    /// @notice custom error e.g error Unauthorized() when decleared outside a contract can be imported into other contracts
+    error Unauthorized(string error);
+    function withdraw() public {
+        if (msg.sender != owner)
+    /// @notice Uses less gass than revert("unathourized");
+            revert Unauthorized("unathourized");
+      owner.transfer(address(this).balance);
+    }
+}
+
+error InsufficientBalance(uint256 available, uint256 required);
+
+contract TestToken {
+    mapping(address => uint) balance;
+    function transfer(address to, uint256 amount) public {
+        if (amount > balance[msg.sender])
+            revert InsufficientBalance({
+                available: balance[msg.sender],
+                required: amount
+            });
+        balance[msg.sender] -= amount;
+        balance[to] += amount;
+    }
+}
---
+++ contracts/safemath.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8;
+
+// safe math
+contract SafeMath {
+    /// @notice This should return an error for undeflow, when using > 0.8.x
+    function Underflow() public pure returns (uint256) {
+        uint256 x = 0;
+        x--;
+        return x;
+    }
+
+    /// @notice This should not return an error for undeflow becasue of unchecked {}, when using > 0.8.x
+    function UncheckedUnderflow() public pure returns (uint256) {
+        uint256 x = 0;
+        unchecked {
+            x--;
+        }
+        return x;
+    }
+}
